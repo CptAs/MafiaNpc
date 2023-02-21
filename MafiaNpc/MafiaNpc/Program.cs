0f3d10f80241aaf35072605162cbdd061f15f6c4
CptAs <arpek2121@gmail.com>
2023-02-21 21:03:57 +0100

    Initial Commit

---
+++ MafiaNpc/MafiaNpc/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MafiaNpc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
